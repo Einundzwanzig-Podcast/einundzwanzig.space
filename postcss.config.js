accc4f3ce3abb6544a226c11a2592e7ee6692a78
Dennis Reimann <mail@dennisreimann.de>
2022-07-19 12:03:07 +0200

    Upgrade dependencies

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,10 @@
-const mediaVariables = require('postcss-media-variables')
-
 module.exports = {
   plugins: [
     require('autoprefixer'),
     require('postcss-import'),
     require('postcss-extend'),
     require('postcss-nesting'),
-    mediaVariables,
     require('postcss-custom-media'),
     require('postcss-calc'),
-    mediaVariables,
   ],
 }
