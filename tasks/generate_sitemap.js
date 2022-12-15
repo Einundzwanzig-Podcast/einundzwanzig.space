630b36f02da3007f0895bf6fb9e35221991efc79
Dennis Reimann <mail@dennisreimann.de>
2022-12-15 20:09:35 +0100

    Base URL environment variable

--- tasks/generate_sitemap.js
+++ tasks/generate_sitemap.js
@@ -3,8 +3,11 @@ const glob = require('glob')
 const { writeFileSync } = require('fs')
 const { resolve } = require('path')
 
+const { DEPLOY_PRIME_URL, URL } = process.env
+const BASE = DEPLOY_PRIME_URL || URL || 'https://einundzwanzig.space'
+
 const html = glob.sync(resolve(__dirname, '..', `dist/**/*.html`))
-const pages = html.map(file => file.replace(/.*\/dist/, 'https://einundzwanzig.space').replace(/index\.html$/, '')).filter(f => !f.endsWith('/kontakt/') && !f.endsWith('/datenschutz/'))
+const pages = html.map(file => file.replace(/.*\/dist/, BASE).replace(/index\.html$/, '')).filter(f => !f.endsWith('/kontakt/') && !f.endsWith('/datenschutz/'))
 const now = (new Date()).toISOString()
 const file = resolve(__dirname, '..', `src/sitemap.pug`)
 const rendered = pug.renderFile(file, { pages, now, pretty: true })
