235fd1680af318969dca44d2a8cb2c83b8228b9b
Dennis Reimann <mail@dennisreimann.de>
2023-03-09 07:13:53 +0100

    Upgrade dependencies

--- tasks/generate_sitemap.js
+++ tasks/generate_sitemap.js
@@ -1,12 +1,12 @@
 const pug = require('pug')
-const glob = require('glob')
+const { globSync } = require('glob')
 const { writeFileSync } = require('fs')
 const { resolve } = require('path')
 
 const { DEPLOY_PRIME_URL, URL } = process.env
 const BASE = DEPLOY_PRIME_URL || URL || 'https://einundzwanzig.space'
 
-const html = glob.sync(resolve(__dirname, '..', `dist/**/*.html`))
+const html = globSync(resolve(__dirname, '..', `dist/**/*.html`))
 const pages = html.map(file => file.replace(/.*\/dist/, BASE).replace(/index\.html$/, '')).filter(f => !f.endsWith('/kontakt/') && !f.endsWith('/datenschutz/'))
 const now = (new Date()).toISOString()
 const file = resolve(__dirname, '..', `src/sitemap.pug`)
