7e78308507febd9a77634403216600c1ec3a0b69
Dennis Reimann <mail@dennisreimann.de>
2023-09-23 11:58:31 +0200

    Fix build

--- tasks/generate_nostr.js
+++ tasks/generate_nostr.js
@@ -1,11 +1,10 @@
-const pug = require('pug')
 const { mkdirSync, writeFileSync } = require('fs')
 const { dirname, resolve } = require('path')
 const { NDKUser } = require('@nostr-dev-kit/ndk')
 const team = require('../content/team.json')
 const { nostr: einundzwanzigNpub } = require('../content/meta.json')
 
-const einundzwanzigHex = new NDKUser({ npub: einundzwanzigNpub }).hexpubkey()
+const einundzwanzigHex = new NDKUser({ npub: einundzwanzigNpub }).hexpubkey
 const names = {
   "_": einundzwanzigHex,
   "einundzwanzig": einundzwanzigHex
@@ -19,7 +18,7 @@ const relays = {
 Object.entries(team).forEach(([key, { nostr: npub }]) => {
   if (!npub) return
   const id = key.replace(/[\s]/g, '_')
-  names[id] = new NDKUser({ npub }).hexpubkey()
+  names[id] = new NDKUser({ npub }).hexpubkey
 })
 
 const dst = resolve(__dirname, '..', 'dist', '.well-known', 'nostr.json')
