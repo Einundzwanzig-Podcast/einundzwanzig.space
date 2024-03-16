b9776c5f9947692b71a930694f867722d4738f75
Dennis Reimann <mail@dennisreimann.de>
2024-03-16 09:06:05 +0100

    Update feed parsing

--- tasks/fetch_feed.js
+++ tasks/fetch_feed.js
@@ -68,7 +68,7 @@ const parseEpisode = e => {
   const duration = e['itunes:duration']
   const enclosure = e.enclosure.__attr
   const [, participantsString] =
-    firstLine.match(/[-–—]\s?(?:(?:von\sund\s)?mit\s)([^.]*)/i) || []
+    firstLine.match(/[-–—]\s?(?:(?:(?:von\sund\s)?mit\s)|(?:gelesen\svon\s))([^.]*)/i) || []
   const participants = participantsString
     ? participantsString
         .replace(/(\s*,\s*|\s*und\s*|\s*&amp;\s*)/gi, '%')
